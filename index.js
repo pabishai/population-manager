353702409573ef53b5c2fd5519a30e84f40f772c
PAbishai <abishai.omari@andela.com>
2019-07-23 12:12:08 +0300

    Add PORT to env

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ dotenv.config();
 databaseConnection(process.env.MONGODB_URI);
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is listening on port 3000");
 });
